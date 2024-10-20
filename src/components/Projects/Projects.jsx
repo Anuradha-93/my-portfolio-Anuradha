ab3d93abf8131460575fa5385c5169feb5a694d0
Anuradha-93 <anuradhadhanai@gmail.com>
2024-10-20 12:33:22 +0530

    Initial commit

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import styles from './Projects.module.css';
 import { motion } from 'framer-motion';
-import ProjectCard from './ProjectCard'; // Separate component for project cards
+import ProjectCard from './ProjectCard';
+import pizzamaniaImg from 'src/assets/images/pizzamania.png';
+import weatherImg from 'src/assets/images/weather.png';
+import todoAppImg from 'src/assets/images/todo-app.png'; 
 
 const projectsData = [
   {
     id: 1,
     title: 'Pizza Mania Static Site',
-    description: 'This is a demo Single Page site using HTML,Internal Css,Javascript',
-    image: '/src/assets/images/pizzamania.png',
+    description: 'This is a demo Single Page site using HTML, Internal CSS, Javascript.',
+    image: pizzamaniaImg,
     link: 'https://anuradha-93.github.io/Pizza-static-site/',
   },
   {
     id: 2,
-    title: 'To-Do-APP',
-    description: 'This app was built using flutter flow.In this we can create,update,delete,display Task of our daily life.',
-    image: '/src/assets/images/to-do.png',
+    title: 'To-Do App',
+    description: 'This app was built using Flutter Flow. You can create, update, delete, and display daily tasks.',
+    image: todoAppImg,
     link: 'https://app.flutterflow.io/run/b1HmrqiyrKg1zpl2bAr4',
   },
   {
     id: 3,
-    title: 'Weather APP',
-    description: 'This app was built using React Js and Weather API.',
-    image: '/src/assets/images/weather.png',
+    title: 'Weather App',
+    description: 'This app was built using React JS and Weather API.',
+    image: weatherImg,
     link: 'https://anuradha-93.github.io/weather-app/',
   },
   
