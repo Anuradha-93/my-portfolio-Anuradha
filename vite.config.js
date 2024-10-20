67b3088aaf8124df5a2f4ae361efe2628505dd3e
Anuradha-93 <anuradhadhanai@gmail.com>
2024-10-20 13:03:24 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base:"/my-portfolio-Anuradha/",
   plugins: [react()],
-})
+  // other configuration options
+});
